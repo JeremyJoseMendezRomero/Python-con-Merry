64b4f685ce9921433866a6e5ccc48a570756599f
JeremyJoseMendezRomero <jeremymendezromero@gmail.com>
2024-10-30 03:13:22 +0100

    Ejercicio 1 - Saludos

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print ('hola mundo')
+# Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+# Ingrese su nombre: Perico
+# Hola, Perico
+#  
+
+name = input('enter your name: ')
+print(f'Hello, {name}')
