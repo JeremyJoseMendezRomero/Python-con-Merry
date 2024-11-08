3cc4f923b9a5e6f9866e7ac96061f55b57b62372
JeremyJoseMendezRomero <jeremymendezromero@gmail.com>
2024-11-08 04:47:09 +0100

    Ejercicio 6 - Pitágoras

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-# Escriba un programa que pida al usuario un entero de tres dígitos, y entregue el número con los dígitos en orden inverso:
+# Escriba un programa que reciba como entrada las longitudes de los dos catetos a y b de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa c del triangulo, dado por el teorema de Pitágoras: c2=a2+b2
 # 
-# Ingrese numero: 345
-# 543
+# .
 # 
-# Ingrese numero: 241
-# 142
+# Ingrese cateto a: 7
+# Ingrese cateto b: 5
+# La hipotenusa es 8.6023252670426267
 
+import math
 
-
-print('A program to get the inverted number that you enter \n')
-
+print('A program to calculate hypotenuse\n')
 
 while True:
     try :
-        number = int(input('Enter the number you want the invert: '))
-        invertedNumber = str(number)[::-1]
-        print(f'Here is the result: {invertedNumber}')
+        cathetusA = float(input('Enter the cathetus A: '))
+        cathetusB = float(input('Enter the cathetus B: '))
+        hypotenuse = math.sqrt(cathetusA ** 2 + cathetusB ** 2)
+
+        print(f'Here is the hypotenuse: {hypotenuse}')
         continueAsk = input( "\nDo you want to do another calculation? (Y/N): " ).strip().lower()
         if continueAsk != "y" :
             print("    Thaks for using the program. Goodbye! \n ")
@@ -28,4 +29,4 @@ while True:
             print("    Thaks for using the program. Goodbye! \n ")
             break
 
-# Exercise 05
+# Exercise 06
