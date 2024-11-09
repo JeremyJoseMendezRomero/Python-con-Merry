bd3c8a9dd1ce1b1da6329d63787abd77007b5891
JeremyJoseMendezRomero <jeremymendezromero@gmail.com>
2024-11-09 04:20:26 +0100

    Ejercicio 7 - Horas futuras

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
-# Escriba un programa que reciba como entrada las longitudes de los dos catetos a y b de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa c del triangulo, dado por el teorema de Pitágoras: c2=a2+b2
+# Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, que indique qué hora marcará el reloj dentro de h horas:
 # 
-# .
+# Hora actual: 3
+# Cantidad de horas: 5
+# En 5 horas, el reloj marcara las 8
 # 
-# Ingrese cateto a: 7
-# Ingrese cateto b: 5
-# La hipotenusa es 8.6023252670426267
+# Hora actual: 11
+# Cantidad de horas: 43
+# En 43 horas, el reloj marcara las 6
 
-import math
+print('A program to calculate what hour will be in n time\n')
 
-print('A program to calculate hypotenuse\n')
+hours = list(range(0, 24, 1))
 
 while True:
     try :
-        cathetusA = float(input('Enter the cathetus A: '))
-        cathetusB = float(input('Enter the cathetus B: '))
-        hypotenuse = math.sqrt(cathetusA ** 2 + cathetusB ** 2)
+        actualTime = int(input('Enter the actual hour (without minutes) in 24 hours format: '))
+        skipHours = int(input('Enter the amount of time you want to skip: '))
+        if actualTime in hours:
+            indexTime = hours.index(actualTime)
 
-        print(f'Here is the hypotenuse: {hypotenuse}')
+            # función .index() para filtrar y ubicar el elemento especificado dentro del array,
+            # se le agrega el parametro actualTime para especificar este valor y esto nos da el indice desde el cual se comenzará el recorrido.
+
+            arrayLength = len(hours)
+
+            # función len() para determinar la cantidad de elementos del parametro hours, 24 elementos en este caso
+
+            indexResult = (indexTime + skipHours) % arrayLength
+
+            # indexResult = (indexTime + skipHours) % arrayLength: este paso es clave. Aquí estamos utilizando aritmética modular para calcular cuál sería el índice final después de recorrer "skipHours" veces, de manera cíclica.
+            # indexTime + skipHours: calcula el número de pasos a partir del índice de inicio.
+            # % arrayLength: con la operación modular (%), nos aseguramos de que el índice sea "envuelto" (ruleta) dentro de los límites del array. Esto simula el comportamiento de moverse en un ciclo.
+
+            # Por ejemplo, si indexTime es 2 y n es 6:
+            # 2 + 6 = 8
+            # 8 % 24 = 0 (ya que el array tiene longitud 24), por lo que el índice resultante es 8.
+
+            skipedHours = hours[indexResult]
+        
+            print(f'in {skipHours} hours, will be: {skipedHours} hours')
+        else:
+            print("escribe en formato 24 h.")
+
         continueAsk = input( "\nDo you want to do another calculation? (Y/N): " ).strip().lower()
         if continueAsk != "y" :
             print("    Thaks for using the program. Goodbye! \n ")
@@ -29,4 +54,4 @@ while True:
             print("    Thaks for using the program. Goodbye! \n ")
             break
 
-# Exercise 06
+# Exercise 07
