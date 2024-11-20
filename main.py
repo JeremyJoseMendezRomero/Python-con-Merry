7f0ce7f7a031384e4c57e759f0f50163a251898f
JeremyJoseMendezRomero <jeremymendezromero@gmail.com>
2024-11-20 04:09:15 +0100

    Ejercicio 9 - Qué nota necesito

--- main.py
+++ main.py
@@ -1,42 +1,29 @@
-# Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
+# Un alumno desea saber que nota necesita en el tercer certamen para aprobar un ramo.
 # 
-# Ingrese un numero: 4.5
-# 0.5
+# El promedio del ramo se calcula con la siguiente formula.
+# NC=(C1+C2+C3) / 3
+# NF=NC⋅0.7+NL⋅0.3
 # 
-# Ingrese un numero: -1.19
-# 0.19
+# Donde NC
+# es el promedio de certámenes, NL el promedio de laboratorio y NF
+# 
+# la nota final.
+# 
+# Escriba un programa que pregunte al usuario las notas de los dos primeros certamen y la nota de laboratorio,
+#  y muestre la nota que necesita el alumno para aprobar el ramo con nota final 60.
+# 
+# Ingrese nota certamen 1: 45
+# Ingrese nota certamen 2: 55
+# Ingrese nota laboratorio: 65
+# Necesita nota 72 en el certamen 3
+
 
 while True:
     try :
-        unfilteredNumber = float(input('Enter a number with it decimals: '))
-        
-        # Esta es la manera minificada de la resolución del ejercicio 8
-        print(round(abs(unfilteredNumber) % 1, len(str(unfilteredNumber)[(len(str(int(abs(unfilteredNumber) // 1))) + 1):])))
-
-        # Ejemplo del caso 14.578
-        # unfilteredNumber // 1 me permíte obtener el entero que sería: 14
-        # Ya que estoy midiendo la cantidad de caracteres con la función len(), agrego un + 1 para contar el punto decimal
-        # Al tener 3 como resultado, procedo a la variable rounderTwo
-        #
-        #rounder = len(str(int(abs(unfilteredNumber) // 1))) + 1
-        #
-        # En rounderTwo, convierto unfilteredNumber a STRING para poder usar la función SLICE[]
-        # Función a la cual se le asigna el parametro [3:] ya que 3 es el valor obtenido en la variable rounder
-        #
-        #rounderTwo = len(str(unfilteredNumber)[rounder:]
-        #
-        # Luego de cortar los caracteres de inicio a fin con SLICE[rounder:] procede a entrar en el siguiente print()
-        # El cual lo convierte nuevamente a un valor numérico y en el parametro de la función round()
-        # Se le agrega el valor de rounderTwo, que contiene la cantidad exacta de decimales ingresados en el input
-        #
-        #print(round(abs(unfilteredNumber) % 1, rounderTwo)))
-        #
-        # Forma de resolverlo con la lógica de David
-        #
-        # dataOne = len(str(int(abs(unfilteredNumber)))) + 1
-        # dataTwo = len(str(unfilteredNumber)[dataOne:])
-        # print(round(abs(unfilteredNumber - int(unfilteredNumber)), dataTwo))
-
+        cOne = float(input('enter your first contest note: '))
+        cTwo = float(input('enter your second contest note: '))
+        nL = float(input('enter your laboratory note: '))
+        print(f'You need {round((3 * (60 - (nL * 0.3)) / 0.7) - cOne - cTwo, 2)} point on your Third note to pass')
 
         continueAsk = input( "\nDo you want to do another calculation? (Y/N): " ).strip().lower()
         if continueAsk != "y" :
@@ -50,4 +37,4 @@ while True:
             print("    Thaks for using the program. Goodbye! \n ")
             break
 
-# Exercise 08
+# Exercise 09
